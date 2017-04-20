da43459c375ddedb3aa832cc546064e3c042dea8
MihajloVitanovic <vitanovic.mihajlo@gmail.com>
2017-04-20 19:03:29 +0200

    Kreirane klase za zadatke fixes #3

---
+++ src/main/java/zadaci/Zadatak4BrisanjeVrednosti.java
@@ -0,0 +1,20 @@
+package zadaci;
+
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+import java.sql.SQLException;
+
+/**
+ * Created by androiddevelopment on 20.4.17..
+ */
+public class Zadatak4BrisanjeVrednosti {
+    public static void main(String[] args) {
+        ConnectionSource connectionSource = null;
+        try {
+            //Potrebno je prvo konektovati se na bazu
+            connectionSource = new JdbcConnectionSource("jdbc:sqlite:knjigaOblast.db");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
