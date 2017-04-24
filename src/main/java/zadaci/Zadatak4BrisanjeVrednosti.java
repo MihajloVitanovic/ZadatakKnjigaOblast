56001e690ab90604056d5fc88b7b3a4f8f23a916
Mihajlo-desktopW10 <vitanovic.mihajlo@gmail.com>
2017-04-25 00:52:48 +0200

    Uradjen zadatak izmena vrednosti

--- src/main/java/zadaci/Zadatak4BrisanjeVrednosti.java
+++ src/main/java/zadaci/Zadatak4BrisanjeVrednosti.java
@@ -1,18 +1,42 @@
 package zadaci;
 
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import model.Knjiga;
+import model.Oblast;
+
 import java.sql.SQLException;
+import java.util.List;
 
 /**
  * Created by androiddevelopment on 20.4.17..
  */
 public class Zadatak4BrisanjeVrednosti {
+    static Dao<Knjiga,Integer> knjigaDao;
+    static Dao<Oblast,Integer> oblastDao;
     public static void main(String[] args) {
         ConnectionSource connectionSource = null;
         try {
             //Potrebno je prvo konektovati se na bazu
             connectionSource = new JdbcConnectionSource("jdbc:sqlite:knjigaOblast.db");
+
+            //Instanciranje Dao objekata
+            knjigaDao = DaoManager.createDao(connectionSource, Knjiga.class);
+            oblastDao = DaoManager.createDao(connectionSource, Oblast.class);
+
+            List<Oblast> oblast=oblastDao.queryForAll();
+            for (Oblast o:oblast)
+                System.out.println("Oblast = " + o);
+
+            List<Oblast> pronadjenaOblast=oblastDao.queryForEq(Oblast.POLJE_NAZIV, "Aritmeticki operatori");
+            Oblast oblastZaIzmenu=pronadjenaOblast.get(0);
+            oblastDao.delete(oblastZaIzmenu);
+
+            oblast=oblastDao.queryForAll();
+            for (Oblast o:oblast)
+                System.out.println("Oblast = " + o);
         } catch (SQLException e) {
             e.printStackTrace();
         }
